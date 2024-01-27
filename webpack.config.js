c16e40fbe0340f63c40ca18dac0abb0f9f410dd8
lucaoskaique <lucas.kaique175@hotmail.com>
2024-01-27 02:03:28 -0300

    Refactor webpack.config.js to use ES modules and update configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-let path = require('path');
+import path from "path";
+import { fileURLToPath } from 'url';
 
-module.exports = {
-    entry: {
-        'index': './src/js/index.js',
-    },
-    output: {
-        filename: '[name].js',
-        path: path.join(__dirname, 'public'),
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
-    }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "production",
+  entry: ["./src/js/index.js"],
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "public"),
+  },
 };
